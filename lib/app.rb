0dccb6426ba2a441fac72dd5d33e3d21dbd82974
Tariok <mingotevan@gmail.com>
2021-10-13 17:53:35 +0200

    oui

--- lib/app.rb
+++ lib/app.rb
@@ -57,22 +57,22 @@
 # while true
 #   begin
 
-# rclient = Twitter::REST::Client.new do |config|
-#   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-#   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-#   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-#   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-# end
+ rclient = Twitter::REST::Client.new do |config|
+ config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+     config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
 
 
     
  
-# sclient = Twitter::Streaming::Client.new do |config|         # topics to watch       
-# config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-# config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-# config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-# config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-# end
+ sclient = Twitter::Streaming::Client.new do |config|         
+ config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+ config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+ config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+ config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+ end
 
 
 # topics = ['#bonjour_monde']         
@@ -88,15 +88,14 @@
 # end
 
 # end
-config = [consumer_key :ENV["TWITTER_CONSUMER_KEY"] ,consumer_secret :ENV["TWITTER_CONSUMER_SECRET"],access_token :ENV["TWITTER_ACCESS_TOKEN"],access_token_secret :ENV["TWITTER_ACCESS_TOKEN_SECRET"]]
-rclient = Twitter::REST::Client.new (config)
-sclient = Twitter::Streaming::Client.new (config)
 
-  topics = ['#bonjour_monde']        
-  sclient.filter(:track => topics) do |tweet|             
+
+      
+  sclient.filter(track: "#bonjour_monde") do |tweet|
     if tweet.is_a?(Twitter::Tweet)               
       puts tweet.text                
-      rclient.fav tweet             
+      rclient.favorite(tweet)   
+      rclient.follow(tweet.user.screen_name)          
     end         
   end
   
